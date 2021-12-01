51063a8425a75b195564ae8a31e8f2808b157de0
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-01 23:40:11 +0530

    [Arjun] Add ; Added Executed

--- BasicCoreProgram/FlipCoin.cs
+++ BasicCoreProgram/FlipCoin.cs
@@ -89,5 +89,16 @@ namespace BasicCoreProgram
                 Console.WriteLine(num + " is not a Prime number");
             }
         }
+        public void SwappingNumber()
+        {
+            Console.WriteLine("Enter the First number :");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number :");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+            Console.WriteLine("After Swapping these Two Numbers : First number is =" + firstNumber + " and Second number is =" + secondNumber);
+        }
     }
 }
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. flipCoin \n 2. Leapyear \n 3. PowofTwo \n 4. Harmonic\n 5. prime factor\n 6. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. flipCoin \n 2. Leapyear \n 3. PowofTwo \n 4. Harmonic\n 5. prime factor\n 6. Swappingnumber\n Exit");
             
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
@@ -34,9 +34,13 @@ namespace BasicCoreProblems
                         BasicPrograms basicPrograms = new BasicPrograms();
                         basicPrograms.Prime();
                         break;
-
-
                     case 6:
+                        BasicPrograms swap = new BasicPrograms();
+                        swap.SwappingNumber();
+                        break;
+
+
+                    case 7:
                         flag = false;
                         break;
                 }
