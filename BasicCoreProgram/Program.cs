6d79fd34d9bcdea9ac1e19d294d28d831d7a23f1
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-01 16:29:20 +0530

    [Arjun 3] Add ; Added learned

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -6,7 +6,7 @@ namespace BasicCoreProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Program number which is to be executed \n 1. flipCoin \n 2. Leapyear \n 3. Exit");
+            Console.WriteLine("Enter the Program number which is to be executed \n 1. flipCoin \n 2. Leapyear \n 3. PowofTwo \n 4. Exit");
             
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
@@ -22,8 +22,13 @@ namespace BasicCoreProblems
                         BasicPrograms check = new BasicPrograms();
                         check.LeapYear();
                         break;
-
                     case 3:
+                        BasicPrograms res = new BasicPrograms();
+                        res.PowerOfTwo();
+                        break;
+
+
+                    case 4:
                         flag = false;
                         break;
                 }
